fb454b44236bc7fdb1bc3cc4353fef6f8c974b84
genjigjj <18812674687@163.com>
2018-04-25 22:02:05 +0800

    修改icon不居中显示的bug

--- src/main.js
+++ src/main.js
@@ -4,9 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-
+import 'vuetify/dist/vuetify.min.css'
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
